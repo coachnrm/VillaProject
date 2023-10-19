0d9430b8fb9e937d86b0a1e749a039c186b12127
home <nuttapong_coach@hotmail.com>
2023-10-19 21:22:43 +0700

    projectproblem migration

---
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Infrastructure.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Villa> Villas { get; set; }
+
+    }
+}
+
---
+++ WhiteLagoon.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WhiteLagoon.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
